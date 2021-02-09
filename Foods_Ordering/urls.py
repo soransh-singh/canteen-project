79023dba2cfb1c53339229bcd4839c4df4ad56df
soransh <53964537+soransh@users.noreply.github.com>
2021-02-09 14:56:59 +0530

    update

--- Foods_Ordering/urls.py
+++ Foods_Ordering/urls.py
@@ -18,6 +18,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.static import serve
+from django.conf.urls import url
+
+
 admin.site.site_header = "Login Administrator"
 admin.site.site_title = "Food-Ordering"
 admin.site.index_title = "Food Ordering Admin Site"
@@ -26,6 +30,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('main.urls')),
     path('accounts/', include('accounts.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
 if settings.DEBUG:
